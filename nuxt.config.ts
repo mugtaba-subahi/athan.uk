e5d88128a87f93c5c8ee5a906536754536bc2545
Mugtaba <m_subahi@hotmail.com>
2022-09-04 14:45:56 +0100

    updated glob

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     plugins: [
       VitePWA({
         workbox: {
-          globPatterns: ["**/*.*}"],
+          globPatterns: ["**/*.*"],
           navigateFallback: null,
           disableDevLogs: false
         }
