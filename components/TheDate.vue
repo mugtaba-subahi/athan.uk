6db09c324d49de85d45a8acf1d0951edd7cd5e5e
Mugtaba <m_subahi@hotmail.com>
2022-11-19 19:25:10 +0000

    update date component at midnight

--- components/TheDate.vue
+++ components/TheDate.vue
@@ -7,11 +7,24 @@
 </template>
 
 <script lang="ts" setup>
-const date = new Date().toLocaleString("en-GB", {
+import { storeToRefs } from "pinia";
+import useStore from "!stores";
+
+const Store = useStore();
+const { dayOfMonth } = storeToRefs(Store);
+
+const dateConfig: Intl.DateTimeFormatOptions = {
   weekday: "short",
   year: "numeric",
   month: "short",
   day: "numeric"
+};
+
+let date = ref(new Date().toLocaleString("en-GB", dateConfig));
+
+// watch - for when application is left open (otherwise wont refresh at midnight)
+watch(dayOfMonth, () => {
+  date.value = new Date().toLocaleString("en-GB", dateConfig);
 });
 </script>
 
