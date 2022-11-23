410ba0a0d781ad842530a8a9ba988f2ab89bb1b7
Mugtaba <m_subahi@hotmail.com>
2022-11-23 04:09:40 +0000

    testing athan audio

--- public/registerSW.js
+++ public/registerSW.js
@@ -9,3 +9,14 @@ const failed = (err) => {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(success, failed);
 }
+
+
+console.log('1111111111111111111111111111XX');
+
+const audio = new Audio('athan.mp3');
+
+console.log(audio);
+console.log('playing audio nowww');
+audio.play();
+
+console.log('2222222222222222222222EEE');
