05a07b6d07e2a70b0034fafc7a3820722977e930
muji <muji@mujis-Mac-mini.local>
2024-09-15 02:43:32 +0100

    added screenshots

--- utils/application.ts
+++ utils/application.ts
@@ -4,26 +4,12 @@ export const forceApplicationRefresh = async (): Promise<void> => {
   deleteCache("data");
   deleteCache("app_version");
 
-
-  console.log('1111');
   if ('serviceWorker' in navigator) {
-    console.log('2222');
-
     const registrations = await navigator.serviceWorker.getRegistrations();
-    console.log('33333');
-    console.log(registrations);
-    
     for (const registration of registrations) {
-      console.log('444');
-      const x = await registration.update();
-      console.log(x);
-      console.log('55555');
-
+      await registration.update();
     }
-    
   }
-
-  console.log('66666');
 
   location.reload();
 };
